52e4d749316b8be947c63899fbed2f8fb683f7cb
Artem Gatalskiy <gatalskiyag@oxagile.com>
2019-06-20 14:40:36 +0300

    implement test project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  title = 'joshsmith-portfolio';
+
+  profile = {
+    name: 'Artem Gatalskiy',
+    position: 'Front-end Developer',
+    githubUrl: 'https://github.com/agatalsqi',
+    photoUrl: 'https://avatars0.githubusercontent.com/u/18549847?s=460&v=4'
+  };
+
+  projects = [
+    {
+      name: 'Multiparts',
+      year: '2018',
+      link: 'https://multiparts.net/'
+    },
+    {
+      name: 'Travelshift',
+      year: '2019',
+      link: 'https://guidetoiceland.is/'
+    },
+    {
+      name: 'GreenAnt',
+      year: '2017',
+      link: 'http://greenant.by/'
+    }
+  ]
 }
